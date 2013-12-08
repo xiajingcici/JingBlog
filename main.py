6d3f665e055779325764b586509899e2acc19b83
xiajingcici <xiajingcici@gmail.com>
2013-12-08 16:32:48 -0500

    template

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cgi
 import urllib
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
@@ -17,34 +22,30 @@ class Greeting(ndb.Model):
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    self.response.out.write('<html><body>')
     guestbook_name = self.request.get('guestbook_name')
     # There is no need to actually create the parent Book entity; we can
     # set it to be the parent of another entity without explicitly creating it
     ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
     greetings = Greeting.query_book(ancestor_key).fetch(20)
 
-    for greeting in greetings:
-      if greeting.author:
-        self.response.out.write(
-            '<b>%s</b> wrote:' % greeting.author.nickname())
-      else:
-        self.response.out.write('An anonymous person wrote:')
-      self.response.out.write('<blockquote>%s</blockquote>' %
-                              cgi.escape(greeting.content))
+    if users.get_current_user():
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
 
-    self.response.out.write("""
-          <form action="/sign" method="post">
-            <input type="hidden" value="%s" name="guestbook_name">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
-          </form>
-          <hr>
-          <form>Guestbook name: <input value="%s" name="guestbook_name">
-          <input type="submit" value="switch"></form>
-        </body>
-      </html>""" % (cgi.escape(guestbook_name), cgi.escape(guestbook_name)))
+    template_values = {
+      'greetings': greetings,
+      'url': url,
+      'url_linktext': url_linktext,
+      'guestbook_name': guestbook_name
 
+    }
+
+    template = jinja_environment.get_template('index.html')
+    self.response.out.write(template.render(template_values))	
+	
 class Guestbook(webapp2.RequestHandler):
   def post(self):
     # Set parent key on each greeting to ensure that each
