b4940ba2d197306f07334da06e7a3e7130fa0ac8
xiajingcici <xiajingcici@gmail.com>
2013-12-11 00:34:51 -0500

    finially worked

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ JingBlog
 Please view on 
 
 https://jingblogost.appspot.com/
+
+http://stackoverflow.com/questions/8634700/python-indentationerror-unexpected-indent
+
+Spent 2 hours to debug this issue!!!!!
+
+Run your program with
+
+python -t script.py
+This will warn you if you have mixed tabs and spaces.
+
+On *nix systems, you can see where the tabs are by running
+
+cat -A script.py
+and you can automatically convert tabs to 4 spaces with the command
+
+expand -t 4 script.py > fixed_script.py
+PS. Be sure to use a programming editor (e.g. emacs, vim), not a word processor, when programming. You won't get this problem with a programming editor.
--- main.py
+++ main.py
@@ -10,57 +10,27 @@ jinja_environment = jinja2.Environment(
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
-class Greeting(ndb.Model):
-  """Models an individual guestbook entry with author, content, and date."""
-  author = ndb.UserProperty()
-  content = ndb.StringProperty()
-  date = ndb.DateTimeProperty(auto_now_add=True)
-
-  @classmethod
-  def query_book(cls, ancestor_key):
-    return cls.query(ancestor=ancestor_key).order(-cls.date)
-
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    guestbook_name = self.request.get('guestbook_name')
-    # There is no need to actually create the parent Book entity; we can
-    # set it to be the parent of another entity without explicitly creating it
-    ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
-    greetings = Greeting.query_book(ancestor_key).fetch(20)
-
-    if users.get_current_user():
-      url = users.create_logout_url(self.request.uri)
-      url_linktext = 'Logout'
+  def get(self):    
+    user = users.get_current_user()
+    if user:
+      url = users.create_logout_url(self.request.uri)	
+      username = user.nickname()	  
+      url_linktext = 'Logout' 
     else:
       url = users.create_login_url(self.request.uri)
+      username = ""
       url_linktext = 'Login'
 
-    template_values = {
-      'greetings': greetings,
+    template_values = {      
       'url': url,
-      'url_linktext': url_linktext,
-      'guestbook_name': guestbook_name
-
+      'username': username,
+      'url_linktext': url_linktext
     }
 
     template = jinja_environment.get_template('templete/temp.html')
     self.response.out.write(template.render(template_values))	
 	
-class Guestbook(webapp2.RequestHandler):
-  def post(self):
-    # Set parent key on each greeting to ensure that each
-    # guestbook's greetings are in the same entity group.
-    guestbook_name = self.request.get('guestbook_name')
-    # There is no need to actually create the parent Book entity; we can
-    # set it to be the parent of another entity without explicitly creating it
-    greeting = Greeting(parent=ndb.Key("Book", guestbook_name or "*notitle*"),
-                        content = self.request.get('content'))
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
-    greeting.put()
-    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
-
 app = webapp2.WSGIApplication([
-  ('/', MainPage),
-  ('/sign', Guestbook)
+  ('/', MainPage)
 ])
